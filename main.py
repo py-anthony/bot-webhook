18f3f66b449ba981a2988607ad468b149d2a1d9f
Anthony Rafidison <100955619+anthonyraf@users.noreply.github.com>
2023-03-08 19:20:26 +0000

    Add webhook

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    data = request.json
+    print("Received webhook : ",data["body"])
+
+    if data["body"]["object"] == "page":
+        return "EVENT_RECEIVED", 200
+    
+    else:
+        return "Not Found", 404
